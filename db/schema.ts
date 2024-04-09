5d9c0437790d8fdb4bbeb2665ac28492cf676a3a
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-04-09 23:58:47 +0100

    Drizzle and Neon

---
+++ db/schema.ts
@@ -0,0 +1,15 @@
+import {
+  boolean,
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+});
